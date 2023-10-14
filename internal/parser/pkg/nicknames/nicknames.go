b7d5d5a8a54d91759a44b559a856b5fb082ba2c8
on3dd <onedeadwave.work@gmail.com>
2023-10-14 13:14:19 +1000

    enhance: improve parser logic

--- internal/parser/pkg/nicknames/nicknames.go
+++ internal/parser/pkg/nicknames/nicknames.go
@@ -1,11 +1,12 @@
 package nicknames
 
 import (
-	"slices"
+	"regexp"
 	"sort"
 	"strings"
 
 	"github.com/gocolly/colly"
+
 	"github.com/on3dd/wot-nicknames-scrapper/pkg/utils"
 )
 
@@ -26,11 +27,11 @@ func ExtractNicknamesFromParagraph(e *colly.HTMLElement) []string {
 func ParseLexemes(words []string) []string {
 	lexemes := make([]string, 0)
 
-	filtered := filterWords(words)
+	// Match redundant symbols and cyrillic letters
+	rx := regexp.MustCompile(`["'\(\)\,]|[\x{0400}-\x{04FF}]`)
 
-	for _, word := range filtered {
-		tokens := stringToTokens(word)
-
+	for _, word := range words {
+		tokens := stringToTokens(word, rx)
 		lexemes = append(lexemes, tokens...)
 	}
 
@@ -39,22 +40,7 @@ func ParseLexemes(words []string) []string {
 	return utils.Unique(lexemes)
 }
 
-func filterWords(words []string) []string {
-	filtered := make([]string, 0)
-
-	for _, item := range words {
-		// Skip blacklisted items
-		if slices.Contains(utils.BlacklistedItems, item) {
-			continue
-		}
-
-		filtered = append(filtered, item)
-	}
-
-	return filtered
-}
-
-func stringToTokens(word string) []string {
+func stringToTokens(word string, rx *regexp.Regexp) []string {
 	for _, sep := range utils.Separators {
 		if strings.Contains(word, sep) {
 			tokens := strings.Split(word, sep)
@@ -62,8 +48,10 @@ func stringToTokens(word string) []string {
 			result := make([]string, 0)
 
 			for _, token := range tokens {
-				if len(token) > 1 {
-					result = append(result, token)
+				processed := rx.ReplaceAllString(token, "")
+
+				if len(processed) > 1 {
+					result = append(result, processed)
 				}
 			}
 
